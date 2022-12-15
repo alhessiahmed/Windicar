5ca3155549ecc825858248e65aa103fd8f8ea151
Ahmed Alhessi <ahmed-hesy@hotmail.com>
2022-12-15 23:56:01 +0200

    fix anonymous can add cars

--- lib/widgets/custom_floating_button.dart
+++ lib/widgets/custom_floating_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:logo/pref/shared_pref_controller.dart';
 import 'package:logo/screens/add_or_update_car.dart';
 import 'package:logo/utils/helpers.dart';
 
@@ -12,12 +13,14 @@ class CustomFloatingButton extends StatelessWidget with Helpers {
     return FloatingActionButton(
       onPressed: () {
         // pickImageSource(context: context);
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => AddOrUpdateCar(),
-          ),
-        );
+        SharedPrefController().loggedIn
+            ? Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AddOrUpdateCar(),
+                ),
+              )
+            : Navigator.pushNamed(context, '/login_screen');
       },
       child: const Icon(Icons.add_rounded),
     );
